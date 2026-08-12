0f0d019b3db00f4c04def0337cc8b2907991bc36
juahhola-del <juahhola@gmail.com>
2026-08-12 09:25:16 -0400

    Preparar despliegue publico Juegazo

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import ReservationClient from "./components/ReservationClient";
 import { getCatalog } from "../lib/catalog-source";
 
+export const dynamic = "force-dynamic";
+
 export default async function HomePage() {
   const catalog = await getCatalog();
   return (
